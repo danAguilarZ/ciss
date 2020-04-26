961629bcfd7f013aa644e89cbe8e5ea485d8b7b1
DANIEL EDUARDO AGUILAR ZUÑIGA <daniel.aguilar.zuniga@gmail.com>
2020-04-26 14:04:36 -0500

    FEAT: Se agrega que sean argumentos de shell el HTML a modificar y el ambiente virtual de python

--- execute_covid_update.sh
+++ execute_covid_update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+virtual_envoronment="${1:-"$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/venv/Scripts/"}"
+html_location="${2:-"$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/test_ciss/index.html"}"
+
 while True
 do
   echo "Descargando pdf"
@@ -7,7 +10,8 @@ do
 
   pdftotext.exe -layout -f 2 -l 8 "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/reporte_covid.pdf"
 
-  source /c/Users/danyg/PycharmProjects/ciss/venv/Scripts/activate
+  source "${virtual_envoronment}activate"
+
   echo "Guardando en base"
   python -m ciss --config "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/config.yml" --reporte "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/reporte_covid.txt" --salida "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida"
 
@@ -24,7 +28,7 @@ do
     caso=$((caso+1));
     IFS='|' read -ra confirmados <<< "$line";
     trim_var=$( echo ${confirmados[1]} | sed ':a;N;$!ba;s/\n/;/g' )
-    sed -i "s/<li>.*<span id=\"mas_confirmados_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${confirmados[0]}<span id=\"mas_confirmados_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" test_ciss/index.html;
+    sed -i "s/<li>.*<span id=\"mas_confirmados_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${confirmados[0]}<span id=\"mas_confirmados_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" "${html_location}";
   done < "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida_casos_confirmados.txt"
 
   caso=0;
@@ -33,7 +37,7 @@ do
     caso=$((caso+1));
     IFS='|' read -ra muertes <<< "$line";
     trim_var=$( echo ${muertes[1]} | sed ':a;N;$!ba;s/\n/;/g' )
-    sed -i "s/<li>.*<span id=\"mas_muertes_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${muertes[0]}<span id=\"mas_muertes_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" test_ciss/index.html;
+    sed -i "s/<li>.*<span id=\"mas_muertes_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${muertes[0]}<span id=\"mas_muertes_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" "${html_location}";
   done < "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida_muertes.txt"
 
   caso=0;
@@ -42,19 +46,19 @@ do
     caso=$((caso+1));
     IFS='|' read -ra letalidad <<< "$line";
     trim_var=$( echo ${letalidad[1]} | sed ':a;N;$!ba;s/\n/;/g' )
-    sed -i "s/<li>.*<span id=\"mas_letalidad_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${letalidad[0]}<span id=\"mas_letalidad_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" test_ciss/index.html;
+    sed -i "s/<li>.*<span id=\"mas_letalidad_${caso}\" class=\"pull-right\"><b>.*<\/b>/<li>${letalidad[0]}<span id=\"mas_letalidad_${caso}\" class=\"pull-right\"><b>${trim_var}<\/b>/g" "${html_location}";
   done < "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida_letalidad.txt"
 
   while IFS= read -r line;
   do
     IFS='|' read -ra global <<< "$line";
     trim_var=$( echo ${global[6]} | sed ':a;N;$!ba;s/\n/;/g' )
-    sed -i "s/<span id=\"total_confirmados_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_confirmados_mundo\" class=\"counter\">${global[1]}<\/span>/g" test_ciss/index.html;
-    sed -i "s/<span id=\"total_nuevos_confirmados_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_nuevos_confirmados_mundo\" class=\"counter\">${global[2]}<\/span>/g" test_ciss/index.html;
-    sed -i "s/<span id=\"total_muertes_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_muertes_mundo\" class=\"counter\">${global[3]}<\/span>/g" test_ciss/index.html;
-    sed -i "s/<span id=\"total_nuevas_muertes_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_nuevas_muertes_mundo\" class=\"counter\">${global[5]}<\/span>/g" test_ciss/index.html;
-    sed -i "s/<span id=\"total_mortalidad_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_mortalidad_mundo\" class=\"counter\">${trim_var}<\/span>/g" test_ciss/index.html;
-    sed -i "s/<span id=\"total_letalidad_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_letalidad_mundo\" class=\"counter\">${global[4]}<\/span>/g" test_ciss/index.html;
+    sed -i "s/<span id=\"total_confirmados_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_confirmados_mundo\" class=\"counter\">${global[1]}<\/span>/g" "${html_location}";
+    sed -i "s/<span id=\"total_nuevos_confirmados_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_nuevos_confirmados_mundo\" class=\"counter\">${global[2]}<\/span>/g" "${html_location}";
+    sed -i "s/<span id=\"total_muertes_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_muertes_mundo\" class=\"counter\">${global[3]}<\/span>/g" "${html_location}";
+    sed -i "s/<span id=\"total_nuevas_muertes_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_nuevas_muertes_mundo\" class=\"counter\">${global[5]}<\/span>/g" "${html_location}";
+    sed -i "s/<span id=\"total_mortalidad_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_mortalidad_mundo\" class=\"counter\">${trim_var}<\/span>/g" "${html_location}";
+    sed -i "s/<span id=\"total_letalidad_mundo\" class=\"counter\">.*<\/span>/<span id=\"total_letalidad_mundo\" class=\"counter\">${global[4]}<\/span>/g" "${html_location}";
 
     break;
   done < "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida_total.txt"
@@ -65,7 +69,7 @@ do
   rm "$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")/salida_total.txt"
 
   current_date=$( date +%Y-%m-%d );
-  sed -i "s/última actualización: .*<\/h4>/última actualización: ${current_date}<\/h4>/g" test_ciss/index.html;
+  sed -i "s/última actualización: .*<\/h4>/última actualización: ${current_date}<\/h4>/g" "${html_location}";
 
   echo "Se ha finalizado el proceso"
 
